481f6a3e887f3656cdc9d97c324b573b9c9f4ead
haltcase <bo@haltcase.dev>
2025-04-11 21:55:43 -0500

    test: restore type morph

--- src/util/getSchemaProperties.test.ts
+++ src/util/getSchemaProperties.test.ts
@@ -33,8 +33,9 @@ test("getSchemaProperties: `_` and `env` are not listed", () => {
 			type({
 				_: "string[]",
 				env: {
-					SOME_FLAG: "string"
-					// .transform((value) => value.toLowerCase() === "true")
+					SOME_FLAG: type("string").pipe(
+						(value) => value.toLowerCase() === "true"
+					)
 				},
 				optionName: "string",
 				maybeFlag: "boolean?"
