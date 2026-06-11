d1932cfea0cc322f39b10a9cbb67ce57e66957b7
haltcase <bo@haltcase.dev>
2026-06-10 23:50:41 -0500

    build: make staged command run eslint selectively
    It was previously running eslint on everything

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,6 @@ export default defineConfig({
 		}
 	},
 	staged: {
-		"*.{js,jsx,ts,tsx,css,yml,yaml}": "pnpm format"
+		"*.{js,jsx,ts,tsx,css,yml,yaml}": ["eslint --fix", "vp check --fix --no-lint"]
 	}
 });
